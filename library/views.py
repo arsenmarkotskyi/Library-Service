a9ea6bc68375905003c75e768c0082c5c410a121
Arsen Markotskyi <arsen.markotskui@gmail.com>
2025-01-05 18:36:42 +0200

    "add base views for  Book, Borrowing, Payment"

--- library/views.py
+++ library/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from library.models import Book, Borrowing, Payment
+from library.serializers import BookSerializer, BorrowingSerializer, PaymentSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BorrowingViewSet(viewsets.ModelViewSet):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingSerializer
+
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
