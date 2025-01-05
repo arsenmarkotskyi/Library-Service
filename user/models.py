eabdd47efeb2bc20d63688e8ed93e841f3e42a82
Arsen Markotskyi <arsen.markotskui@gmail.com>
2025-01-05 18:35:49 +0200

    "add models Book, Borrowing, Payment"

--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    password = models.CharField(max_length=128)
+    is_staff = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.email
