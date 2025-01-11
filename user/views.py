94e1da641019d63e922e80e7e12af60458ff969e
Arsen Markotskyi <arsen.markotskui@gmail.com>
2025-01-11 17:48:27 +0200

    "Implement CRUD for the Users Service"

--- user/views.py
+++ user/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics
+from django.contrib.auth import get_user_model
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import UserSerializer
+
+User = get_user_model()
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = ()
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_object(self):
+        return self.request.user
